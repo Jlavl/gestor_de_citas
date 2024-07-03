45536fbf6b337aafc969a79d1a06d9dbd029fc66
Julian <jelaverallauro@gmail.com>
2024-07-03 13:23:44 -0300

    estructura

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer">
+          Learn React - ESE
         </a>
       </header>
+      <div className="">
+        <div className="contenedor">
+          <div className="one-half">
+            <h1 className="titulo2">CREAR MI CITA</h1>
+
+          </div>
+        </div>
+      </div>
     </div>
+    
     
   );
 }
