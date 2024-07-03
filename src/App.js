69b82acd9e7d8e361b8e58e24b6fd80adda87267
Julian <jelaverallauro@gmail.com>
2024-07-03 14:19:04 -0300

    mas estructura

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+/*      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a className="App-link"
            href="https://reactjs.org"
@@ -12,18 +8,35 @@ function App() {
            rel="noopener noreferrer">
           Learn React - ESE
         </a>
-      </header>
+      </header>*/
+
+function App() {
+  return (
+    <div className="App">
       <div className="">
         <div className="contenedor">
-          <div className="one-half">
-            <h1 className="titulo2">CREAR MI CITA</h1>
-            
+          <div className="one-half column">
+            <h2 className="titulo">CREAR MI CITA</h2>
+            <div className="Formulario">
+              <label>Nombre Mascota</label>
+              <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" ></input>
+              <label>Nombre Dueño</label>
+              <input type="text" name="dueño" class="u-full-width" placeholder="Nombre dueño de la mascota" ></input>
+              <label>Fecha</label>
+              <input type="date" name="fecha" class="u-full-width" ></input>
+              <label>Hora</label>
+              <input type="time" name="fecha" class="u-full-width" ></input>
+            </div>
+          </div>
+          <div className="one-half column">
+            <h2 className="titulo">ADMINISTRA TUS CITAS</h2>
+          <label>Nombre Mascota</label>
+              <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota"></input>
           </div>
         </div>
       </div>
+
     </div>
-    
-    
   );
 }
 
