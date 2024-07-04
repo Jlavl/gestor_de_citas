76a119b5b845ce9890483e8f4716396f22ebf156
Julian <jelaverallauro@gmail.com>
2024-07-04 12:08:46 -0300

    avanzandomediomal

--- src/App.js
+++ src/App.js
@@ -13,29 +13,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="">
-        <div className="contenedor">
-          <div className="one-half column">
-            <h2 className="titulo">CREAR MI CITA</h2>
-            <div className="Formulario">
-              <label>Nombre Mascota</label>
-              <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" ></input>
-              <label>Nombre Dueño</label>
-              <input type="text" name="dueño" class="u-full-width" placeholder="Nombre dueño de la mascota" ></input>
-              <label>Fecha</label>
-              <input type="date" name="fecha" class="u-full-width" ></input>
-              <label>Hora</label>
-              <input type="time" name="fecha" class="u-full-width" ></input>
-            </div>
-          </div>
-          <div className="one-half column">
-            <h2 className="titulo">ADMINISTRA TUS CITAS</h2>
-          <label>Nombre Mascota</label>
-              <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota"></input>
+      <div className="contenedor">
+        <div className="one-half column">
+          <h2 className="titulo">CREAR MI CITA</h2>
+          <div className="Formulario">
+            <label>Nombre Mascota
+            <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" ></input></label>
+            <label>Nombre Dueño
+            <input type="text" name="dueño" class="u-full-width" placeholder="Nombre dueño de la mascota" ></input></label>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+            <label>Fecha
+            <input type="date" name="fecha" class="u-full-width" ></input></label>
+            <label>Hora
+            <input type="time" name="fecha" class="u-full-width" ></input></label>
+            <label>Sintomas
+            <input type="text" name="sintoma" class="u-full-width" ></input></label>
+            <input type="button" name="mandar" class="botonform" value="Enviar"></input>
           </div>
         </div>
+        <div className="one-half column">
+          <h2 className="titulo">ADMINISTRA TUS CITAS</h2>
+        </div>
       </div>
-
     </div>
   );
 }
